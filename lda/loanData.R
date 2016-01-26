1d3e22b74e9f69867433a4f1051d5ee96ddef4de
rishi1226 <agnihotri.rishabh@gmail.com>
2016-01-26 23:37:57 +0100

    problem2

--- lda/loanData.R
+++ lda/loanData.R
@@ -23,7 +23,7 @@ GenData <- function(param.vector, seed = 5341) {
     
     for(class in 1:length(param.vector)) {
         variance <- SigmaXY(param.vector[[class]]$rho, param.vector[[class]]$sd[1], param.vector[[class]]$sd[2])
-        sim.data <- GenBVN(param.vector[[class]]$no, seed = 5341, param.vector[[class]]$mean, variance)
+        sim.data <- GenBVN(param.vector[[class]]$no, 5341, param.vector[[class]]$mean, variance)
         sim.data$class <- rep(param.vector[[class]]$class.name, param.vector[[class]]$n)
         data <- rbind(data, sim.data)
     }
@@ -33,74 +33,78 @@ GenData <- function(param.vector, seed = 5341) {
   return(data)
 }
 
-#Plot simulated data
-PlotGraph <- function(data, title = "", x.label = "", y.label = ""){
-    p <- ggplot(data = data, aes(x = x, y= y)) +
-        geom_point() +
-        scale_x_continuous(name = x.label) +
-        scale_y_continuous(name = y.label) +
-        ggtitle(title)
+#classify data using lda
+Classify <- function(data){
+    
+    data$intercept <- rep(1, nrow(data))
+    
+    X <- as.matrix(data[,c("intercept", "x", "y")])
+    Y <- as.matrix(data[,c("classApproved", "classDenied", "classUndecided")])
+    
+    weights <- solve(t(X)%*%X) %*% t(X) %*% Y
+    
+    
+    pred <- X %*% weights
+    pred.labels <- factor(apply(pred, 1, function(x) which.max(x)), labels = c("Approved", "Denied", "Undecided"))
+    
+    return(list(weights = weights, prediction = pred.labels))
+}
+
+#Plot the decision boundaries
+PlotBoundary <- function(weights,  data){
+    
+    w1 <- weights[, 1]
+    w2 <- weights[, 2]
+    w3 <- weights[, 3]
+    
+    #Calculating points corresponding to boundary lines
+    x1 <- seq(min(data$x), max(data$x), 0.01)
+    y1 <- -((w1[3] - w2[3]) / (w1[2] - w2[2])) * x1 + 
+        ((w2[1] - w1[1]) / (w1[2] - w2[2]))
+    y2 <- -((w3[3] - w1[3]) / (w3[2] - w1[2])) * x1 + 
+        ((w1[1] - w3[1]) / (w3[2] - w1[2]))
+    y3 <- -((w2[3] - w3[3]) / (w2[2] - w3[2])) * x1 + 
+        ((w3[1] - w2[1]) / (w2[2] - w3[2]))
+    
+    
+    boundDF1 <- data.frame(x = x1 , y = y1, Animal=rep("Bound1", length(x1)))
+    boundDF2 <- data.frame(x = x1 , y = y2, Animal=rep("Bound2", length(x1)))
+    boundDF3 <- data.frame(x = x1 , y = y3, Animal=rep("Bound3", length(x1)))
+    boundDF1 <- boundDF1[ boundDF1$y < max(data$y) +10 & boundDF1$y > min(data$y) -10, ]
+    boundDF2 <- boundDF2[ boundDF2$y < max(data$y) +10 & boundDF2$y > min(data$y) -10, ]
+    boundDF3 <- boundDF3[ boundDF3$y < max(data$y) +10 & boundDF3$y > min(data$y) -10, ]
+    
+    p <- ggplot(data = data, aes(x = x, y = y)) + 
+        geom_point(aes(col = class)) +
+        xlab("solvency") +
+        ylab("PI ratio") +
+        theme_bw(base_size = 14, base_family = "Helvetica") + 
+        geom_line(data = boundDF1) +
+        geom_line(data = boundDF2) +
+        geom_line(data = boundDF3) +
+        ggtitle("Decision Boundaries")
+    
     p
 }
 
-
+#-------------------------------------------------------------------------------------------
+#Demo
+#------------------------------------------------------------------------------------------
 param.vector <- list(
-    class1 = list(no = 1000, rho = -0.2, sd = c(2, 8), mean = c(6, 30), class.name = "Approved"),
-    class2 = list(no = 1000, rho = 0.8, sd = c(3.5, 12), mean = c(12, 50), class.name = "Denied"),
-    class3 = list(no = 1000, rho = 0.02, sd = c(1, 3), mean = c(4, 10), class.name = "Undecided")
+    class1 = list(no = 50, rho = -0.7, sd = c(10, 20), mean = c(20, 60), class.name = "Approved"),
+    class2 = list(no = 50, rho = -0.1, sd = c(20, 20), mean = c(40, 80), class.name = "Denied"),
+    class3 = list(no = 50, rho = 0.5, sd = c(15, 12), mean = c(20, 25), class.name = "Undecided")
 )
 
 data <- GenData(param.vector)
-data$intercept <- rep(1, nrow(data))
 
-X <- as.matrix(data[,c("intercept", "x", "y")])
-Y <- as.matrix(data[,c("classApproved", "classDenied", "classUndecided")])
+prediction <- Classify(data)
 
-weights <- solve(t(X)%*%X) %*% t(X) %*% Y
-
-
-pred <- X %*% weights
-pred.labels <- factor(apply(pred, 1, function(x) which.max(x)), labels = c("Approved", "Denied", "Undecided"))
-
-table(data$class, pred.labels)
+#Errors
+table(data$class, prediction$prediction)
 
 
-PlotGraph(data, "Linear Classification", "solvency", "PI ratio")
-#Perform K regressions and classify labels and return coefficiemts and labels
-
-plotBoundary(optim.weights, data)
+PlotBoundary(prediction$weights, data)
 #Plot the decision boundaries
 
-weights =optim.weights
-plotBoundary <- function(weights, data){
-  #Calculating points corresponding to boundary lines
-  x <- seq(min(data$x), max(data$x), 0.01)
-  
-  w.c1 <- weights[,1]
-  w.c2 <- weights[,2]
-  w.c3 <- weights[,3]
-  
-  y1 <- -((w.c1[3] - w.c2[3]) / (w.c1[2] - w.c2[2])) * x + 
-    ((w.c1[1] - w.c2[1]) / (w.c1[2] - w.c2[2]))
-  
-  y2 <- -((w.c2[3] - w.c3[3]) / (w.c2[2] - w.c3[2])) * x + 
-      ((w.c2[1] - w.c3[1]) / (w.c2[2] - w.c3[2]))
-  
-  y3 <- -((w.c1[3] - w.c3[3]) / (w.c1[2] - w.c3[2])) * x + 
-      ((w.c1[1] - w.c3[1]) / (w.c1[2] - w.c3[2]))
-  
-  
-  boundDF1 <- data.frame(x = x , y = y1)
-  boundDF2 <- data.frame(x = x , y = y2)
-  boundDF3 <- data.frame(x = x , y = y3)
-  boundDF1 <- boundDF1[ boundDF1$y < max(data$y) +10 & boundDF1$y > min(data$y) -10, ]
-  boundDF2 <- boundDF2[ boundDF2$y < max(data$y) +10 & boundDF2$y > min(data$y) -10, ]
-  boundDF3 <- boundDF3[ boundDF3$y < max(data$y) +10 & boundDF3$y > min(data$y) -10, ]
-  
-  PlotGraph(data, "Linear Classification", "solvency", "PI ratio") + 
-    geom_line(data = boundDF1) 
-  
-  +
-    geom_line(data = boundDF2) +
-    geom_line(data = boundDF3)
-}
+

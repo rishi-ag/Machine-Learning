f938a642ffb393d5eb5d781d412995f06403cfab
rishi1226 <agnihotri.rishabh@gmail.com>
2016-02-05 15:44:51 +0100

    knn

---
+++ kNN/genData.R
@@ -0,0 +1,199 @@
+library(ggplot2)
+library(mvtnorm)
+library(assertthat)
+
+
+GenData <- function(no.classes, param.vector, save.csv = TRUE, save.pdf = TRUE){
+    #-------------------------------------------------------------
+    #Function returns dataset characterised by parameters passed (see GetSpiral).
+    #Also saves csv file of data and a plot of data if repective flags set to TRUE
+    
+    #check that no.classes and length of parameter list is same
+    assert_that(no.classes == length(param.vector))
+    
+    #generate data
+    data <- data.frame(x = as.numeric(), y = numeric(), type = numeric())
+    
+    #generate data for each class based on the parameters specified 
+    for(i in 1:no.classes) {
+        temp <- GetSpiral(param.vector[[i]]$n, param.vector[[i]]$a, param.vector[[i]]$b,
+                          param.vector[[i]]$theta, param.vector[[i]]$step, param.vector[[i]]$disturbance,
+                          param.vector[[i]]$cov.factor, param.vector[[i]]$class.name)
+        data <- rbind(data, temp)
+        
+    }
+    
+    #import csv if save.csv flag raised
+    if(save.csv == TRUE){
+        write.csv(data, file = "data.csv", row.names = FALSE)
+    }
+    
+    #import pdf of plot if save.pdf flag raised
+    if(save.pdf == TRUE)  {
+        data$type <- as.factor(data$type)
+        pdf("plot.pdf", width=6, height=5)
+        print(PlotGraph(data = data, title = "Cyclones types based spiral distance", 
+                        size = 0.8, x.label = "Demeaned diameter", y.label = "Demeaned diameter"))
+        dev.off()
+    }
+    
+    return(data)
+}
+
+GetSpiral <- function(n, a, b, theta, step, disturbance , cov.factor, class.name) {
+    
+    #function returns data that follows a spiral using the polar coordinates equation polar = a + b * theta (see below)
+    #1.   if n is not divisible disturbances then points returned will be more than n
+    #2.   generally higher the value of parameter b, more the number of perfectly non linearly seperatable 
+    #     classes that can be specified
+    #3.   generally higher the value of parameter a, more the distance seperating different classes 
+    #------------------------------------------------------------------------------
+    #parameter space
+    #------------------------------------------------------------------------------
+    #a     = numerical constant in R; controls turn of the spiral
+    #b     = numerical constant in R controls the distance between arms of the spiral
+    #theta = instial angle in radians of the spiral 
+    #step  = successive increase in theta
+    #disturbance = No of points randomly generated using cartesian coordiantes of a given point as mean
+    #cov.factor = factor by which multiplies by the variance covariance matrix of each point
+    #class.name = Name of class
+    #------------------------------------------------------------------------------
+    
+    n <- round(n/disturbance) #change n by a factor of disurbances
+    data <- data.frame(x = as.numeric(), y = numeric())
+    
+    for(i in 0 :(n -1)) {
+        polar <- a + b * (theta + i * step)
+        coords <- c(polar * cos(theta + i * step), polar * sin(theta + i * step))
+        data.temp <- as.data.frame(rmvnorm(n = disturbance, mean = coords, sigma = cov.factor * diag(2)))
+        data <- rbind(data, data.temp)
+    }
+    
+    names(data) <- c("x", "y")
+    data$type <- rep(class.name, n)
+    data
+}
+
+
+PlotGraph <- function(data, title = "", x.label = "", y.label = "", size = 0.8){
+    p <- ggplot(data = data, aes(x = x, y= y, col = type)) +
+        geom_point(size = size) +
+        scale_x_continuous(name = x.label) +
+        scale_y_continuous(name = y.label) +
+        ggtitle(title)
+}
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+
+
+param.vector <- list(class1 = list(n = 1000, a = 10, b = 20, theta = pi/6, step = 0.03, disturbance = 3,
+                                cov.factor = 10, class.name = "A"),
+                     class2 = list(n = 1000, a = 50, b = 20, theta = pi/6, step = 0.03, disturbance = 3,
+                                cov.factor = 6, class.name = "B"),
+                     class3 = list(n = 1000, a = 100, b = 20, theta = pi/6, step = 0.03, disturbance = 3,
+                                cov.factor = 6, class.name = "C"))
+
+data <- GenData(no.classes = 3, param.vector = param.vector)
+
+# ----
+# Hrvoje's plain 2D XOR 
+# ----
+
+genXOR <- function(noObs=50, seed=1111, saveData=TRUE, savePlot=TRUE) {
+    
+    # load the required libraries
+    library(assertthat)
+    library(mvtnorm)
+    library(ggplot2)
+    
+    # check the inputs
+    assert_that(is.scalar(noObs) && is.double(noObs))
+    assert_that(is.scalar(seed) && is.double(seed))
+    assert_that(is.scalar(saveData) && is.logical(saveData))
+    assert_that(is.scalar(savePlot) && is.logical(savePlot))
+    
+    # defining a function for generating bivariate normal data
+    genBVN <- function(n = 1, muXY = c(0,1), sigmaXY = diag(2)) {
+        rdraws <- rmvnorm(n, mean = muXY, sigma = sigmaXY)
+        return(rdraws)
+    }
+    
+    # generate XOR data and add some simple names
+    set.seed(seed)
+    class1 <- rbind(genBVN(noObs, c(1,1), diag(2)),
+                    genBVN(noObs, c(10,10), diag(2)) )
+    class2 <- rbind(genBVN(noObs, c(1,10), diag(2)),
+                    genBVN(noObs, c(10,1), diag(2)) )
+    dataset <- rbind(cbind(class1, 0), cbind(class2, 1))
+    dataset <- as.data.frame(dataset)
+    colnames(dataset) <- c("x1", "x2", "y")
+    
+    # save the dataset in a CSV format
+    if (saveData) {
+        write.csv(dataset, file="dataset.csv", row.names = FALSE)
+    }
+    
+    # save the plot 
+    if (savePlot) {
+        
+        # generating the plot
+        dataPlot <- 
+            ggplot(data = dataset, 
+                   aes(x = x1, y = x2, color = factor(y))) + 
+            geom_point(size = 2, shape = 4) +
+            xlab("Dimension 1") +
+            ylab("Dimension 2") +
+            theme_bw() +
+            theme(text = element_text(family = "Helvetica")) +
+            scale_color_manual("Class", 
+                               values = c("0" = "blue", "1" = "red"))
+        
+        # saving
+        cairo_pdf("dataPlot.pdf")
+        print(dataPlot) 
+        dev.off()
+    }
+    
+    return(dataset)
+}
+
+genWaves <- function(slice = 0.01, 
+                     saveData = TRUE, 
+                     savePlot = TRUE, 
+                     seed = 12345) {
+    
+    library("ggplot2")
+    
+    get_data <- function(slice, seed = 12345){
+        set.seed(seed)
+        x <- seq(0,2*pi,slice)
+        y <- runif(length(x)) + sin(x)
+        z <- runif(length(x)) + cos(x)
+        data <- data.frame(x1 = rep(x,2), x2 = c(y,z), 
+                           y = c(rep(0,length(y)), rep(1,length(z)) ))
+        return(data)
+    }
+    
+    save_csv <- function(data){
+        write.csv(data, file = "dataset.csv", row.names = FALSE)
+    }
+    
+    get_pdf <- function(data){
+        plot <- ggplot(data = data, aes(x=x1, y=x2, color=factor(y))) +
+            geom_point() +
+            theme_bw()
+        cairo_pdf("dataPlot.pdf", family = "Arial", width = 5.5, height = 5)
+        print(plot)
+        dev.off()
+    }
+    
+    data <- get_data(slice)
+    if (saveData){
+        save_csv(data)
+    }
+    if (savePlot){
+        get_pdf(data)
+    }
+    return(data)
+}
--- kNN/kNN.R
+++ kNN/kNN.R
@@ -1,52 +1,79 @@
 library("assertthat")
 library("mvtnorm")
-library("dplyr")
-
+source("./genData.R")
 #--------------------------------------------------------------------------------
 #calculate distance between a row and given dataset
-calc_dist <- function(row, data, p = 2) {
+CalcDist <- function(row, data, p = 2) {
     #check is length of row is equal to # of columns of data 
     #assert_that(are_equal(length(as.numeric(row)), ncol(data)))
     
-    row_matrix <- matrix( rep(as.numeric(row), each = nrow(data)) , nrow = nrow(data))
+    row.matrix <- matrix( rep(as.numeric(row), each = nrow(data)) , nrow = nrow(data))
     
-    return((rowSums( abs((data - row_matrix))^p )) ^ (1.0/p))
+    return((rowSums( abs((data - row.matrix))^p )) ^ (1.0/p))
 }
 
 #--------------------------------------------------------------------------------
-#Calculates order matrix (neighbors) for training data
-train <- function(trainData, k = 1, p = 2) {
+LabelCount <- function(vec, class.type){
+    freq <- vector()
+    for(class in class.type){
+        freq <- c(freq, sum(vec == class))
+    }
+    return(freq)
+}
+#--------------------------------------------------------------------------------
+#Calculates order matrix (neighbors) for Training data
+Train <- function(features, labels, class.type, k = 1, p = 2) {
     
     #create dist matrix
-    distMat <- apply(trainData[,1:2], 1, function(row) calc_dist(row, trainData[,1:2], p))
+    dist.mat <- apply(features[,1:2], 1, function(row) CalcDist(row, features[,1:2], p))
     
-    #OrderMatrix
-    orderMat <- as.matrix(t(apply(distMat, 1, order))[,2:(k+1)])
+    #order.matrix
+    order.mat <- as.matrix(t(apply(dist.mat, 1, order))[,2:(k+1)])
+    class.mat <- matrix(0, nrow = nrow(order.mat), ncol = ncol(order.mat))
+    for(row in 1:nrow(order.mat)) {
+        for(col in 1:ncol(order.mat)) {
+            class.mat[row, col] <- labels[order.mat[row, col]]
+        }
+    }
     
-    return(orderMat)
+    class.prob <- as.data.frame(t(apply(X = class.mat, MARGIN =  1,
+                                        FUN = function(row){LabelCount(row, class.type)/k})))
+    names(class.prob) <- class.type
+    return(class.prob)
 }
 
 #--------------------------------------------------------------------------------
 #Calculates order matrix (neighbors) for new data
 
-predict <- function(trainData, memory, k = 1, p = 2) {
+Predict <- function(features, memory, labels, class.type, k = 1, p = 2) {
     
     #calculate distance matrix
-    distMat <- apply( trainData[,1:2], 1, function(row) calc_dist(row, memory[,1:2], p))
+    dist.mat <- apply( features[,1:2], 1, function(row) CalcDist(row, memory[,1:2], p))
     
-    #OrderMatrix
-    orderMat <- as.matrix(t(apply(distMat, 2, order))[,1:k])
+    #order.matrix
+    order.mat <- as.matrix(t(apply(dist.mat, 2, order))[,1:k])
     
-    return(orderMat)
+    class.mat <- matrix(0, nrow = nrow(order.mat), ncol = ncol(order.mat))
+    for(row in 1:nrow(order.mat)) {
+        for(col in 1:ncol(order.mat)) {
+            class.mat[row, col] <- labels[order.mat[row, col]]
+        }
+    }
+    
+    class.prob <- as.data.frame(t(apply(X = class.mat, MARGIN =  1,
+                                        FUN = function(row){LabelCount(row, class.type)/k})))
+    names(class.prob) <- class.type
+    return(class.prob)
+
 }
 
 #--------------------------------------------------------------------------------
 #Main function for classifying Data
-kNN_classifier <- function(trainData, memory = NULL, trueClasses, 
+KNNClassifier <- function(features, memory = NULL, labels, 
                            operation = "train", k = 1, p = 2) {
     #Input Validation
-    not_empty(trainData)
-    not_empty(trueClasses)
+    not_empty(features)
+    not_empty(labels)
     is.count(k)
     is.count(p)
     assert_that(k %% 2 == 1)
@@ -54,31 +81,76 @@ kNN_classifier <- function(trainData, memory = NULL, trueClasses,
     assert_that(operation %in% c("train", "predict"))
     if(operation == "predict") not_empty(memory)
     
+    class.type <- unique(labels)
     #Calculate Order Matrix
     if(operation == "train") {
-        orderMat <- train(trainData, k, p)
-    }
-    else {
-        orderMat <- predict(trainData, memory, k, p)
+        class.prob <- Train(features, labels, class.type, k, p)
+    } else {
+        class.prob <- Predict(features, memory, labels, class.type, k, p)
     }
     
     #Predict classes
-    predictedClass <- as.character(apply(orderMat, 1, 
-                                         function(x) names(which.max(table(trueClasses[x])))))
+    predicted.class <- data.frame(t(apply(class.prob, 1, function(row) c(type = class.type[which.max(row)], prob = max(row)))))
     
-    #Calculate error
     error <- accuracy <- NA
     
     if(operation == "train"){
-        error <- table(predictedClass, trueClasses)
-        accuracy <- mean(predictedClass == trueClasses )
-    }
-    else {
-        error <- table(predictedClass, trainData[,3])
-        accuracy <- mean(predictedClass == trainData[,3])
+        error <- table(predicted.class$type, labels)
+        accuracy <- mean(predicted.class$type == labels )
+    } else {
+        
+        error <- table(predicted.class$type, features[,3])
+        accuracy <- mean(predicted.class$prob == features[,3])
+        
+        df <- data.frame(class = predicted.class$type, prob = predicted.class$prob)
+        write.csv(df, file = "predictions.csv", row.names = TRUE)
     }
     
-    return(list(predictedClass = predictedClass,
+    return(list(pred.labels = predicted.class$type,
                 error = error,
-                accuracy = accuracy))
+                accuracy = accuracy,
+                class.prob = predicted.class$prob))
 }
+
+#--------------------------------------------------------------------------------------------------
+#Demo
+
+#create dataset
+features <- genWaves()
+labels <- features$y
+
+#trainig data with best k
+
+k_seq <- seq.int(from = 1, to = 31, by = 2)
+
+train_error <- sapply(k_seq, 
+                      function(k) {
+                          (1 - KNNClassifier(features =  features, labels = labels, operation = "train", k = k)$accuracy)
+                          })
+
+#data frams of errors with differet k
+error.DF <- data.frame(k = k_seq, error = train_error)
+
+#plot
+pdf("error_plot.pdf", width=6, height=5)
+print(ggplot(data = error.DF) +
+          geom_point(aes(x = k, y = error)) +
+          geom_line(aes(x = k, y = error)) +
+          ggtitle("Error Rates with different K") +
+          scale_x_discrete(breaks= k_seq, labels=k_seq))
+dev.off()
+
+#choose best k
+best.k <- error.DF$k[which.min(error.DF$error)]
+
+#train with best k
+summary <- KNNClassifier(features = features, memory = NULL, labels = labels, 
+                         operation = "train", k = best.k, p = 2)
+
+summary$error
+#test new data
+test <- genWaves(slice = 0.05, saveData = TRUE, savePlot = TRUE, seed = 12345)
+test.summary <- KNNClassifier(features = test, memory = features, labels = labels, 
+                         operation = "predict", k = best.k, p = 2)
+
+test.summary$error
